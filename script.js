66859f4f1609ecca4bca16331dc38b392bad7cfb
sounakmaji111-code <sounakmaji111@gmail.com>
2026-01-05 23:24:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling for navbar
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document
+      .querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
